c77ed617637506017739d061056ab53f66e43144
Adam <adam.alrasi@outlook.com>
2022-07-12 21:54:13 +0100

    initialcommit

---
+++ script.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
+
+//   function printAge() {
+//     let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//     console.log(output);
+
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       const firstName = 'Steven';
+//       const str = `Oh, and you're a millienial, ${firstName}`;
+//       console.log(str);
+
+//       function add(a, b) {
+//         return a + b;
+//       }
+
+//       output = 'NEW OUTPUT!';
+//     }
+//     // console.log(add(2, 3));
+//     console.log(output);
+//   }
+//   printAge();
+
+//   return age;
+// }
+
+// const firstName = 'Jonas';
+// calcAge(1991);
+
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = '1991';
+
+
+
+// Funcitons
+function addDecl(a, b) {
+  return a + b;
+};
+
+const addExpr = funciton(a, b){
+    return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+
